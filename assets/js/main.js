ec184c27dba7698a5db4c0f46238c4e9fdd3d09f
shunyavkash-krina-paghadal <krina.paghadal@shunyavkash.com>
2025-03-27 10:06:40 +0530

    COUNTER

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ switchBtn.addEventListener("change", function () {
   }
 });
 
-// HERO-SECTION-JS---
+// ABOUT-SECTION-JS---
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 4,
   spaceBetween: 10,
@@ -41,113 +41,6 @@ var swiper = new Swiper(".mySwiper", {
       spaceBetween: 30,
     },
   },
-});
-
-// COUNTER---
-(function ($) {
-  $.fn.countTo = function (options) {
-    options = options || {};
-
-    return $(this).each(function () {
-      // set options for current element
-      var settings = $.extend(
-        {},
-        $.fn.countTo.defaults,
-        {
-          from: $(this).data("from"),
-          to: $(this).data("to"),
-          speed: $(this).data("speed"),
-          refreshInterval: $(this).data("refresh-interval"),
-          decimals: $(this).data("decimals"),
-        },
-        options
-      );
-
-      // how many times to update the value, and how much to increment the value on each update
-      var loops = Math.ceil(settings.speed / settings.refreshInterval),
-        increment = (settings.to - settings.from) / loops;
-
-      // references & variables that will change with each update
-      var self = this,
-        $self = $(this),
-        loopCount = 0,
-        value = settings.from,
-        data = $self.data("countTo") || {};
-
-      $self.data("countTo", data);
-
-      // if an existing interval can be found, clear it first
-      if (data.interval) {
-        clearInterval(data.interval);
-      }
-      data.interval = setInterval(updateTimer, settings.refreshInterval);
-
-      // initialize the element with the starting value
-      render(value);
-
-      function updateTimer() {
-        value += increment;
-        loopCount++;
-
-        render(value);
-
-        if (typeof settings.onUpdate == "function") {
-          settings.onUpdate.call(self, value);
-        }
-
-        if (loopCount >= loops) {
-          // remove the interval
-          $self.removeData("countTo");
-          clearInterval(data.interval);
-          value = settings.to;
-
-          if (typeof settings.onComplete == "function") {
-            settings.onComplete.call(self, value);
-          }
-        }
-      }
-
-      function render(value) {
-        var formattedValue = settings.formatter.call(self, value, settings);
-        $self.html(formattedValue);
-      }
-    });
-  };
-
-  $.fn.countTo.defaults = {
-    from: 0,
-    to: 0,
-    speed: 1000,
-    refreshInterval: 100,
-    decimals: 0,
-    formatter: formatter,
-    onUpdate: null,
-    onComplete: null,
-  };
-
-  function formatter(value, settings) {
-    return value.toFixed(settings.decimals);
-  }
-})(jQuery);
-
-jQuery(function ($) {
-  // custom formatting example
-  $(".count-number").data("countToOptions", {
-    formatter: function (value, options) {
-      return value
-        .toFixed(options.decimals)
-        .replace(/\B(?=(?:\d{3})+(?!\d))/g, ",");
-    },
-  });
-
-  // start all the timers
-  $(".timer").each(count);
-
-  function count(options) {
-    var $this = $(this);
-    options = $.extend({}, options || {}, $this.data("countToOptions") || {});
-    $this.countTo(options);
-  }
 });
 
 // SLICK-SLIDER
@@ -205,8 +98,6 @@ $(() => {
 
 // FANCYBOX
 
-
-
 // toggle
 
 let header = document.querySelector("header");
@@ -229,3 +120,30 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scrolled");
   }
 });
+
+
+// COUNTER---
+
+function animateCounters() {
+  const counters = document.querySelectorAll(".count-number");
+
+  counters.forEach((counter) => {
+    const target = +counter.getAttribute("data-to");
+    let count = 0;
+    let increment = target / 100;
+
+    function updateCounter() {
+      count += increment;
+      if (count >= target) {
+        counter.innerText = Math.floor(target);
+      } else {
+        counter.innerText = Math.floor(count);
+        requestAnimationFrame(updateCounter);
+      }
+    }
+
+    updateCounter();
+  });
+}
+
+document.addEventListener("DOMContentLoaded", animateCounters);
